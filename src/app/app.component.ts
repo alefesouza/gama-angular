fc67252462827e823e069868d1df94d12e1b5c27
Alefe Souza <contact@alefesouza.com>
2019-07-01 21:41:47 -0300

    Aula 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'gama-angular';
+export class AppComponent implements OnInit {
+  title = 'Alefe';
+  items = ['2', '3', '5', '8', '9'];
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.title = 'Souza';
+    }, 2000);
+
+  }
+
+  novoItem() {
+    const text = prompt('Digite um nome');
+
+    this.items.push(text);
+  }
 }
---
+++ src/app/contador/contador.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
+})
+export class ContadorComponent implements OnInit {
+  contador = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.contador++;
+    }, 1000);
+  }
+
+}
